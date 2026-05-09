9522b790deab106f326b517824dbad83c57cab32
souren-in <sourenroy.in@gmail.com>
2026-05-09 17:50:52 +0530

    chapter4

--- Chapter4/07_Problem7.py
+++ Chapter4/07_Problem7.py
@@ -1,3 +1,4 @@
+# Write a Python program to sum all the items in a list.
 l = [10, 20, 30, 40]
 
 print(sum(l))
--- Chapter4/08_Problem8.py
+++ Chapter4/08_Problem8.py
@@ -1,3 +1,4 @@
+# Write a Python program to count the number of zeros in a tuple.
 a = (7, 0, 8 , 0, 8, 0, 12, 0 )
 
 print(a.count(0))
