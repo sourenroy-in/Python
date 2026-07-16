ed3ba116f549981c8d78a0151af68e34143feb32
souren roy <sourenroy.in@gmail.com>
2026-07-16 19:25:45 +0530

    18

---
+++ NoteBook/problem18.py
@@ -0,0 +1,24 @@
+s = set() #Empty Set
+
+n = input("Enter the number :")
+s.add(int(n))
+
+n = input("Enter the number :")
+s.add(int(n))
+
+n = input("Enter the number :")
+s.add(int(n))
+
+n = input("Enter the number :")
+s.add(int(n))
+
+n = input("Enter the number :")
+s.add(int(n))
+
+n = input("Enter the number :")
+s.add(int(n))
+
+n = input("Enter the number :")
+s.add(int(n))
+
+print(s)
